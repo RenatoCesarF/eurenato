d31069735f26bb6821cf8f90667ed818707a712f
RenatoCesarF <re.fbarcellos@hotmail.com>
2026-06-29 14:08:05 -0400

    Base background header images

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,168 +1,24 @@
-<script setup>
-
+<script setup lang="ts">
+import HeaderImages from '@/components/HeaderImages.vue'
+import HeaderTitle from '@/components/HeaderTitle.vue'
 </script>
 
 <template>
   <header>
     <div class="header-bg">
-
-      <img class="header-image black-white" src="/assets/header/renatos/black-white.png" alt="" />
-      <img class="header-image purple-red" src="/assets/header/renatos/purple-red.png" alt="" />
-      <img class="header-image purple-yellow" src="/assets/header/renatos/purple-yellow.png" alt="" />
-      <img class="header-image red" src="/assets/header/renatos/red.png" alt="" />
-      <img class="header-image yellow" src="/assets/header/renatos/yellow.png" alt="" />
-
-      <div class="header-title">
-        <img class="letter e1" src="/assets/header/letters/E_06.png" alt="" />
-        <img class="letter u" src="/assets/header/letters/U_20.png" alt="" />
-        <img class="letter comma" src="/assets/header/letters/Comma_05.png" alt="" />
-        <img class="letter r" src="/assets/header/letters/R_25.png" alt="" />
-        <img class="letter e2" src="/assets/header/letters/E_25.png" alt="" />
-        <img class="letter n" src="/assets/header/letters/N_05.png" alt="" />
-        <img class="letter a" src="/assets/header/letters/A_47.png" alt="" />
-        <img class="letter t" src="/assets/header/letters/T_05.png" alt="" />
-        <img class="letter o" src="/assets/header/letters/O_26.png" alt="" />
-        <img class="letter flash" src="/assets/header/letters/Flash_02.png" alt="" />
-      </div>
+      <HeaderImages/>
+      <HeaderTitle/>
     </div>
   </header>
 </template>
 
-
 <style scoped>
-  .header-bg {
-    position: relative;
-    background: grey;
-    width: 100vw;
-    max_width: 100vw;
-    height: clamp(520px, 85svh, 900px);
-    overflow: hidden;
-  }
-
-  .header-title {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-
-    width: min(94vw, 1900px);
-    aspect-ratio: 1900 / 560;
-
-    transform: translate(-50%, -48%);
-  }
-
-  .letter {
-    position: absolute;
-    height: auto;
-    user-select: none;
-
-    --rot: 0deg;
-    transform: rotate(var(--rot));
-  }
-
-  .letter:hover {
-    animation: shake 0.6s steps(1, end) infinite;
-  }
-
-  @keyframes shake {
-    0% {
-      transform: translate(0, 0) rotate(var(--rot));
-    }
-
-    25% {
-      transform: translate(-6px, 5px) rotate(calc(var(--rot) - 4deg));
-    }
-
-    50% {
-      transform: translate(6px, -7px) rotate(calc(var(--rot) + 5deg));
-    }
-
-    75% {
-      transform: translate(-4px, -3px) rotate(calc(var(--rot) - 2deg));
-    }
-
-    100% {
-      transform: translate(0, 0) rotate(var(--rot));
-    }
-  }
-
-  /* Eu, Renato */
-  .e1 {
-    left: 0%;
-    top: 3%;
-    width: 14%;
-    z-index: 1;
-    transform: rotate(0deg);
-  }
-
-  .u {
-    left: 12.3%;
-    top: 29%;
-    width: 11%;
-    z-index: 4;
-    transform: rotate(0deg);
-  }
-
-  .comma {
-    left: 19.2%;
-    top: 69%;
-    width: 7.2%;
-    z-index: 10;
-    transform: rotate(3deg);
-  }
-
-  .r {
-    left: 28%;
-    top: 13%;
-    width: 20%;
-    z-index: 3;
-    transform: rotate(0deg);
-  }
-
-  .e2 {
-    left: 43.3%;
-    top: 36%;
-    width: 14.2%;
-    z-index: 6;
-    transform: rotate(7deg);
-  }
-
-  .n {
-    left: 55.5%;
-    top: 25%;
-    width: 14.5%;
-    z-index: 7;
-    transform: rotate(-4deg);
-  }
-
-  .a {
-    left: 67.2%;
-    top: 56%;
-    width: 10.8%;
-    z-index: 8;
-    transform: rotate(3deg);
-  }
-
-  .t {
-    left: 75.6%;
-    top: 20%;
-    width: 12.3%;
-    z-index: 9;
-    transform: rotate(3deg);
-  }
-
-  .o {
-    left: 86.1%;
-    top: 39%;
-    width: 15.8%;
-    z-index: 10;
-    transform: rotate(6deg);
-  }
-
-  .flash {
-    left: 85.4%;
-    top: -5%;
-    width: 7.8%;
-    z-index: 11;
-    transform: rotate(13deg);
-  }
+.header-bg {
+  position: relative;
+  width: 100vw;
+  max-width: 100vw;
+  height: clamp(520px, 90svh, 900px);
+  overflow: hidden;
+  background: black;
+}
 </style>
---
+++ src/components/HeaderImages.vue
@@ -0,0 +1,244 @@
+<script setup>
+
+</script>
+
+<template>
+    <img class="header-image black-white" src="/assets/header/renatos/black-white.png" alt="" />
+    <img class="header-image purple-red" src="/assets/header/renatos/purple-red.png" alt="" />
+    <img class="header-image purple-yellow" src="/assets/header/renatos/purple-yellow.png" alt="" />
+    <img class="header-image red" src="/assets/header/renatos/red.png" alt="" />
+    <img class="header-image blue" src="/assets/header/renatos/blue.png" alt="" />
+    <img class="header-image yellow" src="/assets/header/renatos/yellow.png" alt="" />
+</template>
+
+
+<style scoped>
+.header-image {
+  position: absolute;
+  height: auto;
+  user-select: none;
+  pointer-events: none;
+  transform-origin: center;
+}
+
+/* Rosto grande à esquerda */
+.red {
+  left: -15%;
+  top: -40%;
+  width: 83%;
+  z-index: 1;
+  transform: rotate(-10deg);
+}
+
+/* Rosto rosa/vermelho no topo */
+.purple-red {
+  left: -17%;
+  top: -16%;
+  width: 64%;
+  z-index: 4;
+  transform: rotate(-8deg);
+}
+
+/* Imagem grande amarela/marrom ao fundo, lado direito */
+.yellow {
+  left: 44%;
+  top: -22%;
+  width: 63%;
+  z-index: 2;
+}
+
+/* Foto preto e branco principal */
+.black-white {
+  left: 18%;
+  bottom: -30%;
+  width: 55%;
+  z-index: 10;
+  transform: rotate(0deg);
+}
+
+/* Foto roxa/amarela da direita */
+.purple-yellow {
+  right: -8%;
+  bottom: -40%;
+  width: 46%;
+  z-index: 12;
+  transform: rotate(6deg);
+}
+
+.blue {
+  right: 0%;
+  bottom: -40%;
+  width: 46%;
+  z-index: 2;
+  transform: rotate(6deg);
+}
+
+/* @media (max-width: 768px) { */
+/*   .header-bg { */
+/*     height: clamp(500px, 75svh, 720px); */
+/*   } */
+/* } */
+
+/* ========================= */
+/* TELAS MÉDIAS / TABLET */
+/* ========================= */
+
+@media (max-width: 1100px) {
+  .header-bg {
+    height: clamp(500px, 78svh, 760px);
+  }
+
+  .red {
+    left: -43%;
+    top: -17%;
+    width: 95%;
+    z-index: 5;
+    transform: rotate(-34deg);
+  }
+
+  .purple-red {
+    left: 5%;
+    top: -18%;
+    width: 68%;
+    z-index: 4;
+    transform: rotate(18deg);
+  }
+
+  .yellow {
+    left: 43%;
+    top: -15%;
+    width: 82%;
+    z-index: 2;
+  }
+
+  .black-white {
+    left: 19%;
+    bottom: -5%;
+    width: 50%;
+    z-index: 10;
+    transform: rotate(0deg);
+  }
+
+  .purple-yellow {
+    right: -20%;
+    bottom: -30%;
+    width: 52%;
+    z-index: 12;
+    transform: rotate(6deg);
+  }
+
+  .blue {
+    right: -12%;
+    bottom: -34%;
+    width: 62%;
+    z-index: 2;
+    transform: rotate(6deg);
+  }
+}
+
+
+/* ========================= */
+/* CELULAR */
+/* ========================= */
+
+@media (max-width: 768px) {
+  .header-bg {
+    height: clamp(420px, 68svh, 560px);
+  }
+
+  .red {
+    left: -75%;
+    top: 10%;
+    width: 150%;
+    z-index: 5;
+    transform: rotate(-30deg);
+  }
+
+  .purple-red {
+    left: -8%;
+    top: -33%;
+    width: 78%;
+    z-index: 2;
+    transform: rotate(-20deg);
+  }
+
+  .yellow {
+    left: 42%;
+    top: -22%;
+    width: 125%;
+    z-index: 2;
+  }
+
+  .black-white {
+    left: 10%;
+    bottom: -18%;
+    width: 60%;
+    z-index: 10;
+    transform: rotate(0deg);
+  }
+
+  .purple-yellow {
+    right: -24%;
+    bottom: -32%;
+    width: 62%;
+    z-index: 12;
+    transform: rotate(6deg);
+  }
+
+  .blue {
+    right: -0%;
+    bottom: -0%;
+    width: 80%;
+    z-index: 0;
+    transform: rotate(6deg);
+  }
+}
+
+
+/* ========================= */
+/* CELULAR PEQUENO */
+/* ========================= */
+
+@media (max-width: 480px) {
+  .header-bg {
+    height: clamp(380px, 62svh, 500px);
+  }
+
+  .red {
+    left: -82%;
+    top: 15%;
+    width: 165%;
+  }
+
+  .purple-red {
+    left: -10%;
+    top: -20%;
+    width: 86%;
+  }
+
+  .yellow {
+    left: 38%;
+    top: -10%;
+    width: 140%;
+  }
+
+  .black-white {
+    left: 8%;
+    bottom: -10%;
+    width: 78%;
+  }
+
+  .purple-yellow {
+    right: -35%;
+    bottom: -18%;
+    width: 72%;
+  }
+
+  .blue {
+    right: 16%;
+    bottom: -8%;
+    width: 90%;
+  }
+}
+
+</style>
