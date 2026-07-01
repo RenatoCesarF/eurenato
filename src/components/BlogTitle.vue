19c58a299834aa44ab97cc663bcf1135555e9809
RenatoCesarF <re.fbarcellos@hotmail.com>
2026-07-01 10:47:24 -0400

    Project letters added

--- src/components/BlogTitle.vue
+++ src/components/BlogTitle.vue
@@ -5,6 +5,17 @@
 
 <template>
   <div>
+
+    <div class="project-title">
+      <img class="letter" src="/assets/letters/P_07.png" alt="" />
+      <img class="letter" src="/assets/letters/R_17.png" alt="" />
+      <img class="letter" src="/assets/letters/O_08.png" alt="" />
+      <img class="letter" src="/assets/letters/J_12.png" alt="" />
+      <img class="letter" src="/assets/letters/E_01.png" alt="" />
+      <img class="letter" src="/assets/letters/T_20.png" alt="" />
+      <img class="letter" src="/assets/letters/O_13.png" alt="" />
+      <img class="letter" src="/assets/letters/S_06.png" alt="" />
+    </div>
     <h2>Blog</h2>
   </div>
 </template>
