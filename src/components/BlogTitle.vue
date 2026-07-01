2ecafe661449677fa783f0b4d3ccd01e36356829
RenatoCesarF <re.fbarcellos@hotmail.com>
2026-07-01 10:48:52 -0400

    Blog letters added

--- src/components/BlogTitle.vue
+++ src/components/BlogTitle.vue
@@ -7,14 +7,10 @@
   <div>
 
     <div class="project-title">
-      <img class="letter" src="/assets/letters/P_07.png" alt="" />
-      <img class="letter" src="/assets/letters/R_17.png" alt="" />
-      <img class="letter" src="/assets/letters/O_08.png" alt="" />
-      <img class="letter" src="/assets/letters/J_12.png" alt="" />
-      <img class="letter" src="/assets/letters/E_01.png" alt="" />
-      <img class="letter" src="/assets/letters/T_20.png" alt="" />
-      <img class="letter" src="/assets/letters/O_13.png" alt="" />
-      <img class="letter" src="/assets/letters/S_06.png" alt="" />
+      <img class="letter" src="/assets/letters/B_21.png" alt="" />
+      <img class="letter" src="/assets/letters/L_04.png" alt="" />
+      <img class="letter" src="/assets/letters/O_01.png" alt="" />
+      <img class="letter" src="/assets/letters/G_22.png" alt="" />
     </div>
     <h2>Blog</h2>
   </div>
